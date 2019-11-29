367a589b4edf9f064f265ef8e765d48c8aeb8632
高智强 <253974227@qq.com>
2019-11-29 21:43:21 +0800

    与运算

---
+++ learn11_29_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int i = 0;
+	int a = 0, b = 2, c = 3, d = 4;
+	i = a++||++b||c++||d++;//或运算，当第一个为真，则后面全部不进行
+	printf("a=%d\nb=%d\nc=%d\nd=%d", a,b,c,d);
+	system("pause");
+	return 0;
+}
