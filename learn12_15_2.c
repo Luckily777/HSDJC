d970de7215ba386487996d9af2a3dcfed77b63bb
高智强 <253974227@qq.com>
2019-12-15 21:33:54 +0800

    一瓶饮料一元，两个瓶盖换一瓶，20元可以喝多少

---
+++ learn12_15_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int n = 20;
+	int sum = 0;
+	int cover = 0;
+	for (n; n > 0; n /=2){
+		sum += n;
+		if (n % 2 != 0){
+			cover++;
+			if (cover == 2){
+				sum++;
+				cover -= 2;
+			}
+		}
+	}
+	printf("∫»¡À%d∆ø\n £”‡%d∏ˆ∆ø∏«", sum,cover);
+
+	return 0;
+}
