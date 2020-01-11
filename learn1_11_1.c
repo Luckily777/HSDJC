a7a677ebf73028d49dd5bfce7444136341ad772c
高智强 <253974227@qq.com>
2020-01-11 15:15:05 +0800

    整体的截断和提升

---
+++ learn1_11_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("a=%d,b=%d,c=%d", a, b, c);
+	system("pause");
+	return 0;
+}
+
+
---
+++ learn1_11_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char a = 128;
+	printf("a=%u\n", a);
+	system("pause");
+	return 0;
+}
