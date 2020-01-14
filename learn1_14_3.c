69d070d83dbec62b8d98c43fe6c338ac7b5b9b19
高智强 <253974227@qq.com>
2020-01-14 19:53:42 +0800

    寒假刷题练习：判断语句

---
+++ learn1_14_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a = 2, b = 1, c = 2;
+	if (a<b)
+	if (b<0)
+		c = 0;
+	else c += 1;
+	printf("%d", c);
+	system("pause");
+	return 0;
+}
