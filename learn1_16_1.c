c17febd82704bc67e2bc9e6b695be328ed2e5321
高智强 <253974227@qq.com>
2020-01-16 22:19:40 +0800

    寒假练习题1

---
+++ learn1_16_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int i = 0, a = 0;
+	while (i < 20)
+	{
+		for (;;)
+		{
+			if ((i % 10) == 0)break;
+			else i--;
+		}
+		i += 11;
+		a += i;
+	}
+	printf("%d\n", a);
+	system("pause");
+	return 0;
+}
