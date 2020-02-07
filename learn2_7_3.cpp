91097ab72eb8c8929d807753abd43bb0ed007ba6
高智强 <253974227@qq.com>
2020-02-07 21:04:21 +0800

    寒假练习题：2.7.3

---
+++ learn2_7_3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int i = 0;
+int fun(int n)
+{
+	static int a = 2;
+	a++;
+	return(a * n);
+}
+
+int main()
+{
+	int k = 5;
+	{
+		int i = 2;
+		k += fun(i);
+	}
+	k += fun(i);
+	cout << k;
+	system("pause");
+	return 0;
+}
