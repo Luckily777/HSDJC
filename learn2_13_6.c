7d2fdaf7bed4fe2f5ed5be14ad22a7fb3835e8b9
高智强 <253974227@qq.com>
2020-02-13 22:16:47 +0800

    寒假练习题：2.13.6 逻辑运算

---
+++ learn2_13_6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int x = 0, y = 5, z = 3;
+
+	while (z-->0 && ++x<5)
+
+	{
+
+		y = y - 1;
+
+	}
+
+	printf("%d, %d, %d\n", x, y, z);
+	system("pause");
+	return 0;
+}
