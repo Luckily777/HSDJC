60f0741eaea9b80fb722f72235b42f08dd8a8bf8
高智强 <253974227@qq.com>
2020-02-16 22:20:16 +0800

    寒假练习题：2.16.3

---
+++ learn2_16_3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	int value;
+	value= 2;
+	System.out.print(value);
+	System.out.print(value++);
+	System.out.print(value);
+	system("pause");
+	return 0;
+}
