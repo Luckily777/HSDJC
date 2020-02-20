db4fcb8c49714f0bc2180b356bb5975e56c8f6e8
高智强 <253974227@qq.com>
2020-02-20 15:48:05 +0800

    指针的学习2.20.3

---
+++ learn2_20_3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+//指针数组
+void test1(int** arr, int n)//  常用
+{
+	printf("%d", sizeof(arr));
+}
+
+void test2(int *arr[10], int n)//arr是指针 C语言传参不存在传数组的问题
+{
+	printf("%d", sizeof(arr));
+}
+
+void test3(int* arr[], int n)
+{
+	printf("%d", sizeof(arr));
+}
+
+int main(){
+	int *arr[10] = { 0 };//指针数组
+	test1(arr, 10);
+	test2(arr, 10);
+	system("pause");
+	return 0;
+}
