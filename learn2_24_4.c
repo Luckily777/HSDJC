dc962341bbdb9f93a05d7833740590aa73d7b17d
高智强 <253974227@qq.com>
2020-02-24 20:38:31 +0800

    2.24.4 练习题

---
+++ learn2_24_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//cLIANXI
+int main()
+{
+	char ch1, ch2;
+
+	ch1 = 'D' + '8'-'3';
+	ch2 = '9'-'1';
+
+	printf("%c %d\n", ch1, ch2);
+	system("pause");
+	return 0;
+}
