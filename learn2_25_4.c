0e50d0f969290f37a411465dfaa30210eb7e3fff
高智强 <253974227@qq.com>
2020-02-25 21:42:58 +0800

    unsigned char 2.25.4

---
+++ learn2_25_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+//unsigned char
+
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("a=%d,b=%d,c=%d", a, b, c);
+	system("pause");
+	return 0;
+}
