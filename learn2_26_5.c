a0f64a67d0ef0ba6fe099c51635832ebb3a61517
高智强 <253974227@qq.com>
2020-02-26 20:47:03 +0800

    2.26.5 杨辉三角形

---
+++ learn2_26_5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+//杨辉三角形
+void PrintArray(int* str, int n) {
+	int i;
+	for (i = 1; i < n; i++) {
+		printf("%d ", str[i]);
+	}
+	printf("\n");
+}
+
+void PrintYh(int n) {
+	int form[20] = { 1 };
+
+	int i, j;
+	printf("1\n");
+	for (i = 1; i < n; i++) {
+		printf("1 ");
+		for (j = i; j > 0; j--) {//从后往前，开始逐次加，以一维数组的形式输出杨辉三角形
+			form[j] += form[j - 1];
+		}
+		PrintArray(form, i + 1);
+	}
+}
+
+int main() {
+	PrintYh(10);
+	system("pause");
+	return 0;
+}
