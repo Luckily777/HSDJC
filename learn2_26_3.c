b1007851fad3ab17d29ee9fa9c26ce03373db22d
高智强 <253974227@qq.com>
2020-02-26 20:42:58 +0800

    提升和截断2.26.3

---
+++ learn2_26_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Á·Ï°Ìâ2.26.3 ÌáÉıºÍ½Ø¶Ï
+
+
+int main()
+{
+	unsigned char a = 200;
+	unsigned char b = 100;
+	unsigned char c = 0;
+	c = a + b;
+	printf(" %d\n%d\n", a + b, c);
+	system("pause");
+	return 0;
+}
