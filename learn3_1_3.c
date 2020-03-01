d2818ffbc97d9987a8e571cad8a07b6ef8086b3e
高智强 <253974227@qq.com>
2020-03-01 21:07:51 +0800

    练习题：3.1.3

---
+++ learn3_1_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct st_t {
+    int status;
+    short* pdata;
+    char errstr[32];
+};
+
+int main()
+{
+    struct st_t st[16];
+    char* p = (char*)(st[2].errstr + 32);
+    printf("%d", (p - (char*)(st)));
+    system("pause");
+    return 0;
+}
