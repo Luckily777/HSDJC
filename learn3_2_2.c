30c98b5e5c9b51a7541d3f1f51a709ca8672c7dd
高智强 <253974227@qq.com>
2020-03-02 21:47:59 +0800

    练习题：3.2.2

---
+++ learn3_2_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//
+
+int main()
+{
+    char c1, c2;
+    c1 = 'C' + '8'-'3';
+    c2 = '9'-'0';
+    printf("%c %d\n", c1, c2);
+
+    system("pause");
+    return 0;
+}
