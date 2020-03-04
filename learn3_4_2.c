dbd69825fd6db113ecc958b568ec44ff5846ab24
高智强 <253974227@qq.com>
2020-03-04 22:05:03 +0800

    练习题 3.4.2

---
+++ learn3_4_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+//
+int main() {
+    char str1[] = "hello bit.";
+    char str2[] = "hello bit.";
+    char* str3 = "hello bit.";
+    char* str4 = "hello bit.";
+    if (str1 == str2)
+        printf("str1 and str2 are same\n");
+    else
+        printf("str1 and str2 are not same\n");
+
+    if (str3 == str4)
+        printf("str3 and str4 are same\n");
+    else
+        printf("str3 and str4 are not same\n");
+    system("pause");
+    return 0;
+}
