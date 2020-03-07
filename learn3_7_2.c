226d5c3d12ceb8af9a0f501dfcae6d23012e162f
高智强 <253974227@qq.com>
2020-03-07 12:37:47 +0800

    练习题3.7.2

---
+++ learn3_7_2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+//¡∑œ∞Ã‚
+
+int main() {
+    int i = 010, j = 10;
+    printf("%d,%d\n", ++i, j--);
+    system("pause");
+    return 0;
+}
