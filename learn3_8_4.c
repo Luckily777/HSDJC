9d878bc6bfd234d068df960e6fd51e8256ed3c12
高智强 <253974227@qq.com>
2020-03-08 22:29:54 +0800

    练习题：3.8.4

---
+++ learn3_8_4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+//¡∑œ∞Ã‚
+
+int main() {
+	int a = 5, b = 0, c = 0;
+	if (a = b + c) printf("***\n");
+	else  printf("$$$\n");
+	system("pause");
+	return 0;
+}
