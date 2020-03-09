54c5bfd333c3348b5ea9ad4b889357f8447e29b7
高智强 <253974227@qq.com>
2020-03-09 21:59:59 +0800

    练习题3.9.4

---
+++ learn3_9_4.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+//
+class A
+{
+public:
+    A() :m_iVal(0) { test(); }
+    virtual void func() { std::cout << m_iVal << ' '; }
+    void test() { func(); }
+public:
+    int m_iVal;
+};
+class B : public A
+{
+public:
+    B() { test(); }
+    virtual void func()
+    {
+        ++m_iVal;
+        std::cout << m_iVal << ' ';
+    }
+};
+
+
+int main(int argc, char* argv[]) {
+    A* p = new B;
+    p->test();
+    system("pause");
+    return 0;
+}
