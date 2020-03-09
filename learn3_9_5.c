5730dead6f434933010b3896d235bbd356b3eafc
高智强 <253974227@qq.com>
2020-03-09 22:00:26 +0800

    练习题3.9.5

---
+++ learn3_9_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5 
+#define M1 N*3 
+#define M2 N*2 
+
+
+int main() {
+    int i;
+    i = M1 + M2;
+    printf("%d\n", i);
+    system("pause");
+    return 0;
+}
