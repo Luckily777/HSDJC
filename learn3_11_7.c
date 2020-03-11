1ce7b42cb17f6cd25e63b44cf03a6446302154fb
高智强 <253974227@qq.com>
2020-03-11 22:34:43 +0800

    练习题3.11.7

---
+++ learn3_11_7.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//#include <iostream>
+//using namespace std;
+
+
+int main() {
+    unsigned char i = 1;
+    i -= 4;
+    printf("the value of i is : % d", i);
+    system("pause");
+    return 0;
+}
