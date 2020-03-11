4221d8fafc4cf1578f97f2a730a4b84bcc240666
高智强 <253974227@qq.com>
2020-03-11 22:32:46 +0800

    练习题3.11.3

---
+++ learn3_11_3.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c;
+    a= b = c = 0;
+    int x = 35;
+    if (!a) x--;
+    else if (b);
+    if (c) x = 3;
+    else x = 4;
+    printf("%d", x);
+    system("pause");
+    return 0;
+}
