88a52720f39f73dc938f50cf8263bd7faf521a62
高智强 <253974227@qq.com>
2020-03-12 22:04:05 +0800

    练习题：3.12.2

---
+++ learn3_12_2.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//#include <iostream>
+//using namespace std;
+
+struct st_t
+{
+    int status;
+    short* pdata;
+    char errstr[32];
+};
+
+int main() {
+    struct st_t st[16];
+    char* p = (char*)(st[2].errstr + 32);
+    printf("%d", (p - (char*)(st)));
+    system("pause");
+    return 0;
+}
