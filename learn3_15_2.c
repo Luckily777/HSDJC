29838b68ee6153f4790e41f16f83b0d8ba32dbfb
高智强 <253974227@qq.com>
2020-03-15 22:44:33 +0800

    关于对 qsort 的使用

---
+++ learn3_15_2.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//关于对 qsort 的使用
+
+//#include <iostream>
+//using namespace std;
+
+int values[] = { 40, 10, 100, 90, 20, 25 };
+
+int compare(const void* a, const void* b)
+{
+  return (*(int*)a - *(int*)b);
+}
+
+int main() {
+   int n;
+   qsort(values, 6, sizeof(int), compare);
+   for (n = 0; n < 6; n++) {
+       printf("%d ", values[n]);
+   }
+    system("pause");
+    return 0;
+}
