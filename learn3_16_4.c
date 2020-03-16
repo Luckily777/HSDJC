464c40575a733944a94ffdabe2ef8a9c65587ae8
高智强 <253974227@qq.com>
2020-03-16 14:11:51 +0800

    练习题3.16.4

---
+++ learn3_16_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <iostream>
+//using namespace std;
+
+
+
+int main() {
+    long long a = 1, b = 2, c = 3;
+    printf("%d %d %d\n", a, b, c);
+    system("pause");
+    return 0;
+}
