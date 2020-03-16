aaab8ccfbb1cd38b1fa7d4eb39452a99d304a0a0
高智强 <253974227@qq.com>
2020-03-16 14:11:02 +0800

    练习题3.16.2

---
+++ learn3_16_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <iostream>
+//using namespace std;
+
+
+
+int main() {
+    int a = 2, b = 2, c = 2;
+    printf("%d\n", a / b & c);
+    system("pause");
+    return 0;
+}
