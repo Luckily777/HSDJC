6c855a4281d6a3d131c982dbceed5af2865a8be4
高智强 <253974227@qq.com>
2020-03-17 11:35:02 +0800

    练习题3.17.5

---
+++ learn3_17_5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a = 2;
+    int b = ++a;
+    cout << a / 6 << endl;
+    system("pause");
+    return 0;
+}
