1a9b822d06c92f194a935ed49cd54701396bc466
高智强 <253974227@qq.com>
2020-03-18 22:26:10 +0800

    练习题3.18.5

---
+++ learn3_18_5.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+//#include <iostream>
+//using namespace std;
+
+
+
+int main() {
+    int x=127, z;
+    short y=-9;
+    z = x + y;
+    printf("%x %x %x\n",x,y,z);
+    system("pause");
+    return 0;
+}
