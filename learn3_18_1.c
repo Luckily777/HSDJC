c39bccecb89f7d702a9c088a6e130ae6e02739c5
高智强 <253974227@qq.com>
2020-03-18 22:23:12 +0800

    练习题3.18.1

---
+++ learn3_18_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <iostream>
+//using namespace std;
+
+
+int main() {
+    int a = -3;
+    unsigned int b = 2;
+    long c = a + b;
+    printf("%ld\n", c);
+    system("pause");
+    return 0;
+}
