876b890f4493c8f304604cbcba059039efb33df2
高智强 <253974227@qq.com>
2020-03-25 22:53:01 +0800

    练习题：3.25.5

---
+++ learn3_25_5.c
@@ -0,0 +1,24 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+//#include <iostream>
+//using namespace std;
+
+void f(int n);
+
+void f(int n)
+{
+	printf("%d\n", n);
+}
+int main() {
+	/*char str[][10] = { "China","Beijing" }, * p = str;
+	printf("%s\n", p + 10);*/
+
+		void f(int n);
+		f(5);
+
+
+	system("pause");
+	return 0;
+}
